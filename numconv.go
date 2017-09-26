e259b52b6fa785638fdfb00cd885717f54c9f01b
dafengge0913 <dafengge0913@hotmail.com>
2017-09-26 12:41:19 +0800

    Chinese to Arabic

--- numconv.go
+++ numconv.go
@@ -1,5 +1,7 @@
 package numconv
 
+import "fmt"
+
 var chnNumChar = []string{"零", "一", "二", "三", "四", "五", "六", "七", "八", "九"}
 var chnUnitSection = []string{"", "万", "亿", "万亿"}
 var chnUnitChar = []string{"", "十", "百", "千"}
@@ -48,5 +50,45 @@ func sectionToChinese(section int) string {
 		unit++
 		section = section / 10
 	}
+	return result
+}
+
+var chnNumCharToArabMap = map[rune]int{'零': 0, '一': 1, '二': 2, '三': 3, '四': 4, '五': 5, '六': 6, '七': 7, '八': 8, '九': 9}
+
+type chnUnit struct {
+	value     int
+	isSecUnit bool
+}
+
+var chnUnitCharMap = map[rune]chnUnit{'十': {10, false},
+	'百': {100, false}, '千': {1000, false},
+	'万': {10000, true}, '亿': {100000000, true}}
+
+func ChineseToArabic(num string) int {
+	curNum := 0
+	section := 0
+	result := 0
+	for _, r := range num {
+		n, ok := chnNumCharToArabMap[r]
+		if ok {
+			// 是数字
+			curNum = n
+		} else {
+			unit, ok := chnUnitCharMap[r]
+			if !ok {
+				panic(fmt.Sprintf("Unknown rune: '%c' in string: \"%s\"", r, num))
+			}
+			if unit.isSecUnit {
+				// 是节权位 说明一节结束
+				section = (section + curNum) * unit.value
+				result += section
+				section = 0
+			} else {
+				section += curNum * unit.value
+			}
+			curNum = 0
+		}
+	}
+	result += curNum + section
 	return result
 }
--- numconv_test.go
+++ numconv_test.go
@@ -18,3 +18,33 @@ func TestArabicToChinese(t *testing.T) {
 func testArabicToChinese(t *testing.T, n int) {
 	t.Log(n, "=", ArabicToChinese(n))
 }
+
+func BenchmarkArabicToChinese(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		ArabicToChinese(i)
+	}
+}
+
+func TestChineseToArabic(t *testing.T) {
+	for _, s := range []string{"零", "一", "二", "三", "四", "五", "六", "七", "八", "九", "一十"} {
+		testChineseToArabic(t, s)
+	}
+	testChineseToArabic(t, "四十三")
+	testChineseToArabic(t, "二百零三")
+	testChineseToArabic(t, "一万零二十三")
+	//testChineseToArabic(t, "一万零啊二十三") // !!! panic !!!
+	testChineseToArabic(t, "五万四千三百二十一")
+	testChineseToArabic(t, "一亿零五百零七")
+	testChineseToArabic(t, "一百亿五千")
+	testChineseToArabic(t, "一百亿零一百万")
+}
+
+func testChineseToArabic(t *testing.T, s string) {
+	t.Log(s, "=", ChineseToArabic(s))
+}
+
+func BenchmarkChineseToArabic(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		ChineseToArabic(ArabicToChinese(i))
+	}
+}
